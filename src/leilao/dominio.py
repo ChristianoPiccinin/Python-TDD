bc5f2bc055a00366e71860d5e3ce791a9ccfe6b5
Christiano Piccinin <christiano.piccinin@gmail.com>
2021-07-28 18:01:27 -0300

    exclusão do fonte principal.py, adicionado metodo setUp

--- src/leilao/dominio.py
+++ src/leilao/dominio.py
@@ -23,6 +23,9 @@ class Leilao:
         self.descricao = descricao
         self.__lances = []
 
+    def propoe(self, lance: Lance):
+        self.__lances.append(lance)
+
     @property
     def lances(self):
         return self.__lances
@@ -34,10 +37,10 @@ class Avaliador:
         self.maior_lance = sys.float_info.min
         self.menor_lance = sys.float_info.max
 
-    def avalia(self,leilao: Leilao):
+    def avalia(self, leilao: Leilao):
+
         for lance in leilao.lances:
             if lance.valor > self.maior_lance:
                 self.maior_lance = lance.valor
             if lance.valor < self.menor_lance:
                 self.menor_lance = lance.valor
-
